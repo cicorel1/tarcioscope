5f298f4778442914560ad670f72f9f58ffa36231
Tarcio Saraiva <tarcio@gmail.com>
2018-02-08 13:16:17 +1100

    use cookiecutter to setup application

--- README.txt
+++ README.txt
@@ -11,3 +11,30 @@
 ```
 
 > A simple tool to look at the sky with a PiKon Telescope (a newtonian tube).
+
+Getting Started
+---------------
+
+- Change directory into your newly created project.
+
+    cd tarcioscope
+
+- Create a Python virtual environment.
+
+    python3 -m venv env
+
+- Upgrade packaging tools.
+
+    env/bin/pip install --upgrade pip setuptools
+
+- Install the project in editable mode with its testing requirements.
+
+    env/bin/pip install -e ".[testing]"
+
+- Run your project's tests.
+
+    env/bin/pytest
+
+- Run your project.
+
+    env/bin/pserve development.ini
---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'plaster_pastedeploy',
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'picamera'
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='tarcioscope',
+    version='0.0.1',
+    description='A simple tool to look at the sky with a PiKon Telescope (a newtonian tube).',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='Tarcio Saraiva',
+    author_email='tarcio@gmail.com',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = tarcioscope:main',
+        ],
+    },
+)
