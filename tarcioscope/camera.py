8a7f53057f50f06f2e7f0c0722036c0350d94b68
Tarcio Saraiva <tarcio@gmail.com>
2018-02-13 21:18:07 +1100

    refactor camera

--- tarcioscope/camera.py
+++ tarcioscope/camera.py
@@ -2,24 +2,50 @@ import picamera
 
 from struct import Struct
 from time import sleep, time
+from datetime import datetime
 
-WIDTH = 640
-HEIGHT = 480
+METER_MODE = 'spot'
+RESOLUTION = 'FHD'
 FRAMERATE = 24
-JSMPEG_MAGIC = b'jsmp'
-JSMPEG_HEADER = Struct('>4sHH')
-VFLIP = False
-HFLIP = False
+VFLIP = True
+HFLIP = True
 
 
 class Camera(object):
     def __init__(self):
-        with picamera.PiCamera() as self.camera:
-            self.camera.resolution = (WIDTH, HEIGHT)
-            self.camera.framerate = FRAMERATE
-            self.camera.vflip = VFLIP # flips image rightside up, as needed
-            self.camera.hflip = HFLIP # flips image left-right, as needed
-            sleep(1) # camera warm-up time
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = RESOLUTION
+        self.camera.meter_mode = METER_MODE
+        self.camera.framerate = FRAMERATE
+        self.camera.vflip = VFLIP # flips image rightside up, as needed
+        self.camera.hflip = HFLIP # flips image left-right, as needed
+        self.camera.start_preview()
+ 
+    def start_streaming(self, websocket):
+        print('Starting video capture')
+        self.camera.start_recording(websocket.connection, format='mjpeg', resize='HD')
+        while True:
+            self.camera.wait_recording(1)
 
-    def start(self, outputstream):
-        self.camera.start_recording(outputstream, format='mjpeg')
+    def stop_streaming(self):
+        print('Stopping video capture')
+        self.camera.stop_recording()
+
+    def snap(self):
+        print('Camera recording status: %s' % self.camera.recording)
+
+        if self.camera.recording:
+            print('Stopping camera', end='...')
+            self.stop_streaming()
+            print('done!')
+
+        file_name = '/tmp/%s.png' % datetime.now().strftime('%Y%m%d%H%M%S')
+        print('Capturing image to "%s"' % file_name)
+
+        sleep(2)
+
+        self.camera.capture(file_name, format='png')
+
+# camera = Camera()
+# camera.start_streaming('/tmp/myvideo.h264')
+# camera.snap()
--- tarcioscope/main.py
+++ tarcioscope/main.py
@@ -4,20 +4,26 @@ from ws4py.websocket import WebSocket
 from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
 
 from camera import Camera
+from broadcast_output import BroadcastOutput
 
-cherrypy.config.update({ 'server.socket_port': 9000 })
+cherrypy.config.update({ 'server.socket_host': '0.0.0.0', 'server.socket_port': 9000 })
 WebSocketPlugin(cherrypy.engine).subscribe()
 cherrypy.tools.websocket = WebSocketTool()
 
 class StreamingWebSocket(WebSocket):
+    def __init__(self):
+        self.camera = Camera()
+
     def opened(self):
-        # self.send(JSMPEG_HEADER.pack(JSMPEG_MAGIC, WIDTH, HEIGHT), binary=True)
-        self.send()
+        while True:
+            output = BroadcastOuput(self.camera.camera)
+            self.camera.start_streaming(self)
 
 class App(object):
-    def __init__(self):
-        self.picamera = Camera()
-
+    @cherrypy.expose
+    def index(self):
+        return """<video src="/ws"></video>"""
+      
     @cherrypy.expose
     def ws(self):
         cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
