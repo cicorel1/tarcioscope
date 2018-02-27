c2a3cccbc58bcdc925e6fc30ba0219d271061dbe
Tarcio Saraiva <tarcio@gmail.com>
2018-02-27 22:45:52 +1100

    use correct logger

--- tarcioscope/pi_camera_wrapper.py
+++ tarcioscope/pi_camera_wrapper.py
@@ -1,6 +1,6 @@
-import logging
 import picamera
 
+from logger import log
 from datetime import datetime
 
 METER_MODE = 'spot'
@@ -20,25 +20,25 @@ class PiCameraWrapper(object):
         self.camera.vflip = VFLIP
         self.camera.hflip = HFLIP
         self.camera.start_preview()
-        logging.info('Camera setup: Resoltuion[%s] | Meter mode[%s] | Frame rate[%s] | Exposure mode[%s] | ISO[%s]', resolution, meter_mode, FRAMERATE, exposure_mode, iso)
+        log('Camera setup: Resolution[%s] | Meter mode[%s] | Frame rate[%s] | Exposure mode[%s] | ISO[%s]' % (resolution, meter_mode, FRAMERATE, exposure_mode, iso))
 
     def start_streaming(self, output):
-        logging.info('Starting video capture')
+        log('Starting video capture')
         self.camera.start_recording(output, format='yuv')
 
     def stop_streaming(self):
-        logging.info('Stopping video capture')
+        log('Stopping video capture')
         self.camera.stop_recording()
 
     def snap(self):
-        logging.info('Camera recording status: %s' % self.camera.recording)
+        log('Camera recording status: %s' % self.camera.recording)
 
         if self.camera.recording:
-            logging.info('Stopping camera')
+            log('Stopping camera')
             self.stop_streaming()
-            logging.info('Camera stopped!')
+            log('Camera stopped!')
 
         file_name = '/tmp/%s.png' % datetime.now().strftime('%Y%m%d%H%M%S')
-        logging.info('Capturing image to "%s"', file_name)
+        log('Capturing image to "%s"' % file_name)
 
         self.camera.capture(file_name, format='png')
