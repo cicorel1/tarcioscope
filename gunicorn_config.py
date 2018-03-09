40018d4d89eed0bff5e2a22c7a593ce8db6ed87d
Tarcio Saraiva <tarcio@gmail.com>
2018-03-09 15:44:38 +1100

    move stuff around; remove setup.py; include requirements file

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-bind = '0.0.0.0:8000'
-# workers = multiprocessing.cpu_count()
-# worker_connections = 4
-preload = True
-pidfile = 'tarcioscope.pid'
-pythonpath = 'tarcioscope'
