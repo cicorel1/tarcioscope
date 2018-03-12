03fdc0cf19e049e02e1669873010850a18a36865
Tarcio Saraiva <tarcio@gmail.com>
2018-03-13 07:14:32 +1100

    do not use modules modifier; use relative import on wrapper

--- tarcioscope/lib/pi_camera_wrapper.py
+++ tarcioscope/lib/pi_camera_wrapper.py
@@ -1,8 +1,8 @@
 import picamera
 from datetime import datetime
 
-from logger import log
-from broadcast_output import BroadcastOutput
+from . import logger
+from . import broadcast_output
 
 FRAME_WIDTH = 320
 FRAME_HEIGHT = 240
@@ -16,36 +16,36 @@ class PiCameraWrapper(object):
             self.camera.framerate = 24
             self.camera.exposure_mode = 'auto'
             self.camera.iso = 100
-            log('##### CAMERA SETUP #####')
-            log('# Resolution[%s x %s]' % self.camera.resolution[:2])
-            log('# Meter mode[%s]' % self.camera.meter_mode)
-            log('# Exposure mode[%s]' % self.camera.exposure_mode)
-            log('# ISO[%s]' % self.camera.iso)
-            log('########################')
+            logger.log('##### CAMERA SETUP #####')
+            logger.log('# Resolution[%s x %s]' % self.camera.resolution[:2])
+            logger.log('# Meter mode[%s]' % self.camera.meter_mode)
+            logger.log('# Exposure mode[%s]' % self.camera.exposure_mode)
+            logger.log('# ISO[%s]' % self.camera.iso)
+            logger.log('########################')
 
 
         def start_streaming(self):
-            log('Starting video capture')
+            logger.log('Starting video capture')
             self.stop_streaming()
 
-            broadcast_output = BroadcastOutput(self)
-            self.camera.start_recording(broadcast_output, format='yuv')
+            boutput = broadcast_output.BroadcastOutput(self)
+            self.camera.start_recording(boutput, format='yuv')
 
 
         def stop_streaming(self):
-            log('Stopping video capture...')
+            logger.log('Stopping video capture...')
             if not self.camera.recording:
                 self.camera.stop_recording()
             else:
-                log('Camera already stopped. Doing nothing.')
+                logger.log('Camera already stopped. Doing nothing.')
 
 
         def snap(self):
-            log('Camera recording status: %s' % self.camera.recording)
+            logger.log('Camera recording status: %s' % self.camera.recording)
             self.stop_streaming()
 
             file_name = '/tmp/%s.png' % datetime.now().strftime('%Y%m%d%H%M%S')
-            log('Capturing image to "%s"' % file_name)
+            logger.log('Capturing image to "%s"' % file_name)
 
             self.camera.resolution = 'FHD'
             self.camera.capture(file_name, format='png')
